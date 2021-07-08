0d5c906889f76a0fe853f42fe09b4a712100d6ba
Satvik Tiwari <satviktiwarikivtas7@gmail.com>
2021-07-08 17:13:02 +0530

    added clik integration

--- Puzzle Solver/Assets/PencilController.cs
+++ Puzzle Solver/Assets/PencilController.cs
@@ -1,10 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class PencilController : MonoBehaviour
 {
     [SerializeField] Animator animator;
+    [SerializeField] GameObject confetti;
+    [SerializeField] CinemachineVirtualCamera finishCam;
+    [SerializeField] Animator levelComplete;
+    [SerializeField] GameObject objectiveText;
+    [SerializeField] GameObject continueButton;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +21,22 @@ public class PencilController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (this.animator.GetCurrentAnimatorStateInfo(0).IsName("Finish"))
+        {
+            // Avoid any reload.
+            finishCam.Priority = 100;
+            StartCoroutine(LevelComplete());
+        }
+    }
+
+    private IEnumerator LevelComplete()
+    {
+        objectiveText.SetActive(false);
+        yield return new WaitForSeconds(1f);
+
+        continueButton.SetActive(true);
+        levelComplete.gameObject.SetActive(true);
+        levelComplete.SetTrigger("PopUp");
+        confetti.SetActive(true);
     }
 }
---
+++ Puzzle Solver/Assets/SceneLoader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] float transitionTime = 0f;
+    // Start is called before the first frame update
+    
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        StartCoroutine(LoadScene(currentSceneIndex + 1));
+    }
+
+    private IEnumerator LoadScene(int sceneIdx)
+    {
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(sceneIdx);
+      //  Debug.Log("Done");
+    }
+
+    public void LoadFirstScene()
+    {
+        StartCoroutine(LoadScene(0));
+    }
+}
