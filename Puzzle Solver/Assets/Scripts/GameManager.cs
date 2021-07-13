7fc3b7cdde23a38b28b5ccb01e030220c435ea0e
Dhirajlal Vishwakarma <47329713+dhiraj7894@users.noreply.github.com>
2021-07-13 19:41:34 +0530

    update

---
+++ Puzzle Solver/Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject Pencile;
+    public GameObject TrailPath;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Pencile.GetComponent<Animator>().enabled = false;
+        TrailPath.GetComponent<Animator>().enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Pencile.GetComponent<Animator>().enabled = true;
+            TrailPath.SetActive(false);
+        }
+    }
+}
